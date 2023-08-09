34fd81d59243e3edf13f8e46445bddc86335c135
konstantinVolodko <volodko2000@bk.ru>
2023-08-09 17:37:30 +0300

    footer

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -10665,7 +10665,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.querySelector('.main-news__swiper')) {
         new Swiper(".main-news__swiper", {
             slidesPerView: 3,
-    
+            spaceBetween: 30,
         });
     }
 
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.querySelector('.main-news__swiper')) {
         new Swiper(".main-news__swiper", {
             slidesPerView: 3,
-    
+            spaceBetween: 30,
         });
     }
 
