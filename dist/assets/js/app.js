f56bf775c322333ad832ad810752f797db8222ce
konstantinVolodko <volodko2000@bk.ru>
2023-08-10 18:03:43 +0300

    main adaptive + second page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -10669,4 +10669,26 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    let language = document.querySelector('.header__language a.active')
+    let languageAll = document.querySelectorAll('.header__language a:not(.active)')
+
+    if (window.matchMedia("(max-width: 1330px)").matches) {
+        // language.preventDefault()
+        languageAll.forEach(e => {
+            language.nextElementSibling.appendChild(e)
+        })
+    }
+    
+    language.addEventListener('click', e => {
+        e.preventDefault()
+        console.log()
+        language.nextElementSibling.classList.toggle('active')
+        // if (language.classList.contains('active')) {
+        //     language.nextElementSibling.style.height = `${language.nextElementSibling.querySelector('a').scrollHeight}px`
+        // }else {
+        //     language.nextElementSibling.style.height = "0px"
+        // }
+        
+    })
+
 })
